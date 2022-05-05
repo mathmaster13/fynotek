650652961dda9be77902522de327d171eafdb0a5
mathmaster13 <>
2022-05-04 21:30:17 -0400

    added two new suffixes and fixed a bracket

--- dict/src/util/build.sh
+++ dict/src/util/build.sh
@@ -3,7 +3,8 @@
 cd ../..
 rm -rf assets data app.js index.html
 cd src
+mkdir src/assets
 touch src/assets/temp
 npm run build
 cp -rp dist/* ..
-rm -rf dist ../assets/temp src/assets/temp
+rm -rf dist ../assets src/assets
